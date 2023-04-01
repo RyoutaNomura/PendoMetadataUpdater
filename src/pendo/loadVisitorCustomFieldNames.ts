85f7e8c3bda24662c7634e560e20d20f914ff33e
Ryouta Nomura <ryouta.nomura@gmail.com>
2023-04-01 14:39:29 +0900

    Introduce inquerer

--- src/pendo/loadVisitorCustomFieldNames.ts
+++ src/pendo/loadVisitorCustomFieldNames.ts
@@ -1,22 +1,35 @@
-import { PendoMetadataKind } from "../types";
+import { PendoMetadataKind } from "../types.js";
 
-async function loadVisitorCustomFieldNames({ integrationKey }: { integrationKey: string }) {
-    const json = await apiGetV1MetadataSchemaKind({ integrationKey: integrationKey, kind: 'visitor' });
-    return Object.keys(json.custom);
+async function loadVisitorCustomFieldNames({
+  integrationKey,
+}: {
+  integrationKey: string;
+}) {
+  const json = await apiGetV1MetadataSchemaKind({
+    integrationKey: integrationKey,
+    kind: "visitor",
+  });
+  return Object.keys(json.custom);
 }
 
-async function apiGetV1MetadataSchemaKind({ integrationKey, kind }: { integrationKey: string, kind: PendoMetadataKind }) {
-    const END_POINT = `https://app.pendo.io/api/v1/metadata/schema/${kind}`;
-    const res = await fetch(END_POINT, {
-        method: 'GET',
-        headers: new Headers({
-            "content-type": "application/json",
-            "x-pendo-integration-key": integrationKey
-        }),
-        redirect: 'follow'
-    });
-    console.log(`Status: ${res.status} ${res.statusText}`);
-    return await res.json();
+async function apiGetV1MetadataSchemaKind({
+  integrationKey,
+  kind,
+}: {
+  integrationKey: string;
+  kind: PendoMetadataKind;
+}) {
+  const END_POINT = `https://app.pendo.io/api/v1/metadata/schema/${kind}`;
+  const res = await fetch(END_POINT, {
+    method: "GET",
+    headers: new Headers({
+      "content-type": "application/json",
+      "x-pendo-integration-key": integrationKey,
+    }),
+    redirect: "follow",
+  });
+  console.log(`Status: ${res.status} ${res.statusText}`);
+  return await res.json();
 }
 
 export default loadVisitorCustomFieldNames;
--- src/pendo/saveVisitorCustomValues.ts
+++ src/pendo/saveVisitorCustomValues.ts
@@ -1,22 +1,44 @@
-import { VisitorMetadata, AccountMetadata, PendoMetadataKind } from "../types";
- 
-async function saveVisitorCustomValues({ integrationKey, postData }: { integrationKey: string, postData: Array<VisitorMetadata> }) {
-    await apiPostV1MetadataKindGroupValue({ integrationKey: integrationKey, kind: 'visitor', data: postData });
+import {
+  VisitorMetadata,
+  AccountMetadata,
+  PendoMetadataKind,
+} from "../types.js";
+
+async function saveVisitorCustomValues({
+  integrationKey,
+  postData,
+}: {
+  integrationKey: string;
+  postData: Array<VisitorMetadata>;
+}) {
+  await apiPostV1MetadataKindGroupValue({
+    integrationKey: integrationKey,
+    kind: "visitor",
+    data: postData,
+  });
 }
 
-async function apiPostV1MetadataKindGroupValue({ integrationKey, kind, data }: { integrationKey: string, kind: PendoMetadataKind, data: Array<VisitorMetadata | AccountMetadata> }) {
-    const END_POINT = `https://app.pendo.io/api/v1/metadata/${kind}/custom/value`;
-    const res = await fetch(END_POINT, {
-        method: 'POST',
-        headers: new Headers({
-            "content-type": "application/json",
-            "x-pendo-integration-key": integrationKey
-        }),
-        body: JSON.stringify(data),
-        redirect: 'follow'
-    });
-    console.log(`Status: ${res.status} ${res.statusText}`);
-    console.log(await res.text());
+async function apiPostV1MetadataKindGroupValue({
+  integrationKey,
+  kind,
+  data,
+}: {
+  integrationKey: string;
+  kind: PendoMetadataKind;
+  data: Array<VisitorMetadata | AccountMetadata>;
+}) {
+  const END_POINT = `https://app.pendo.io/api/v1/metadata/${kind}/custom/value`;
+  const res = await fetch(END_POINT, {
+    method: "POST",
+    headers: new Headers({
+      "content-type": "application/json",
+      "x-pendo-integration-key": integrationKey,
+    }),
+    body: JSON.stringify(data),
+    redirect: "follow",
+  });
+  console.log(`Status: ${res.status} ${res.statusText}`);
+  console.log(await res.text());
 }
 
 export default saveVisitorCustomValues;
